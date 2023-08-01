f4b583fd147e7c213fc50f4b07736e0efbf347f5
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2023-08-01 13:21:03 +0200

    fix

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ brew install chezmoi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+chezmoi init --apply --source="${script_dir}"
