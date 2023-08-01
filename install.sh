c61a5ab85abfbec636c753d09632b6a91d511db3
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2023-08-01 18:22:06 +0200

    Fix

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e # -e: exit on error
 
+GITHUB_USERNAME="alexsanduk"
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -11,4 +13,4 @@ brew install chezmoi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-chezmoi init --apply --source="${script_dir}"
+chezmoi init --apply --apply ${GITHUB_USERNAME}
